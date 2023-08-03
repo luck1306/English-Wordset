e358a1022b10411786878fdcd6623e7a3726399b
cumulus <luck1306@naver.com>
2023-08-03 15:46:06 +0900

    update errorcode's status code update

--- src/main/java/com/example/englishwordset/error/ErrorCode.java
+++ src/main/java/com/example/englishwordset/error/ErrorCode.java
@@ -9,8 +9,8 @@ public enum ErrorCode {
     USER_NOT_FOUND("user not found exception", 404),
     USER_ALREADY_EXIST("user already exist in db", 400),
     PASSWD_NOT_MATCHED("password is incorrect", 409),
-    TOKEN_TYPE_WRONG("token type is wrong", 400),
-    TOKEN_EXPIRED("token is expired", 403);
+    TOKEN_TYPE_WRONG("token type is wrong", 401),
+    TOKEN_EXPIRED("token is expired", 401);
 
     private final String message;
     private final Integer code;
