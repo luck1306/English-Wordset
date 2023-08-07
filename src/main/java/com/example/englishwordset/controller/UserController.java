0d0f038a468d2289167030b25ac7221758bce1c9
cumulus <luck1306@naver.com>
2023-08-07 15:40:01 +0900

    update change endpoint

--- src/main/java/com/example/englishwordset/controller/UserController.java
+++ src/main/java/com/example/englishwordset/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/user")
 @RestController
 public class UserController {
 
